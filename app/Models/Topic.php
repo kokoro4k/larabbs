873480f7b64b3d38a1aee4d05e083613afba33fd
vagrant <vagrant@homestead>
2017-11-08 08:22:47 +0000

    Seeding topics and users data

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -2,7 +2,12 @@
 
 namespace App\Models;
 
-class Topic extends Model
-{
+/**
+ * Class Topic
+ * 话题数据模型
+ * @package App\Models
+ */
+class Topic extends Model {
+
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
 }
