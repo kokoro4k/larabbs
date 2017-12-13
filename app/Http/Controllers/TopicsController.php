011b6b797564ced038ac34277eef67c9c72a2298
vagrant <vagrant@homestead>
2017-12-13 09:17:39 +0000

    Sidebar active users

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 use App\Models\Topic;
 use App\Models\Category;
+use App\Models\User;
 use Auth;
 use Illuminate\Http\Request;
 
@@ -16,9 +17,10 @@ class TopicsController extends Controller {
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-    public function index(Request $request, Topic $topic) {
+    public function index(Request $request, Topic $topic, User $user) {
         $topics = $topic->withOrder($request->order)->paginate(20);
-        return view('topics.index', compact('topics'));
+        $active_users = $user->getActiveUsers();
+        return view('topics.index', compact('topics', 'active_users'));
     }
 
     public function show(Request $request, Topic $topic) {
--- app/Models/User.php
+++ app/Models/User.php
@@ -15,6 +15,8 @@ class User extends Authenticatable {
         notify as protected laravelNotify;
     }
 
+    use Traits\ActiveUserHelper;
+
     public function notify($instance) {
         // 如果要通知的人是当前用户，就不必通知了！
         if ($this->id == Auth::id()) {
