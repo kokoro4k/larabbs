558ef74f3ed83f0d734a9ef8075da95ca558525d
vagrant <vagrant@homestead>
2018-07-20 06:44:52 +0000

    categories index

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,8 @@ use Illuminate\Http\Request;
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
-    'namespace' => 'App\Http\Controllers\Api',
-    'middleware' => 'serializer:array'
+    'namespace'  => 'App\Http\Controllers\Api',
+    'middleware' => 'serializer:array',
 ], function ($api) {
     $api->group([
         'middleware' => 'api.throttle',
@@ -47,13 +47,15 @@ $api->version('v1', [
     });
     $api->group([
         'middleware' => 'api.throttle',
-        'limit' => config('api.rate_limits.access.limit'),
-        'expires' => config('api.rate_limits.access.expires'),
+        'limit'      => config('api.rate_limits.access.limit'),
+        'expires'    => config('api.rate_limits.access.expires'),
     ], function ($api) {
         // 游客可以访问的接口
+        $api->get('categories', 'CategoriesController@index')
+            ->name('api.categories.index');
 
         // 需要 token 验证的接口
-        $api->group(['middleware' => 'api.auth'], function($api) {
+        $api->group(['middleware' => 'api.auth'], function ($api) {
             // 当前登录用户信息
             $api->get('user', 'UsersController@me')
                 ->name('api.user.show');
